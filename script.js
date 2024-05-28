74b5036ba76e0cbe2a05bf6a9fd812b97676caf2
R. Sam Craig <103201653+Samcraig77@users.noreply.github.com>
2024-05-28 12:38:21 -0700

    started assigning vars to document IDs

---
+++ script.js
@@ -0,0 +1,21 @@
+const amount = document.getElementById("amount");
+const submitBtn = document.getElementById("submit-btn");
+const meters = document.getElementsById("meters");
+const feet = document.getElementsById("feet");
+const liters = document.getElementsById("liters");
+const gallons = document.getElementsById("gallons");
+const kilos = document.getElementsById("kilos");
+const pounds = document.getElementsById("pounds");
+const initialValue = document.getElementsByClassName("initial")
+
+// amount.addEventListener("change", function() {
+// amount.getValue()
+// console.log(amount);
+// })
+
+// amount.addEventListener("oninput", function() {
+
+//     console.log(amount.value)
+//     return amount.value
+//     })
+    
